f2a5ef17929d7611f256714bdd5780ff9cff4f7e
Vatsal Sanchala <98870768+bevatsal1122@users.noreply.github.com>
2022-07-10 23:19:13 +0530

    Create index.js

---
+++ Node.js/index.js
@@ -0,0 +1,9 @@
+// import bevatsal1122 from "./hello.js"
+const bevatsal1122 = require("./hello.js")
+console.log("\nHello Vatsal\n", bevatsal1122);
+
+
+// To use ED6 Sytanx, Javascript File type should be Module.
+// It should have extension .mjs
+// You cannot use CommonJS Syntax (require) in .mjs files
+// You cannot use ES^ Syntax (import) in Normal Javascript Files
