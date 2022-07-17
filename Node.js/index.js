82731b0e12f2a628db0761422a56f3819af1353c
Vatsal Sanchala <98870768+bevatsal1122@users.noreply.github.com>
2022-07-17 16:06:24 +0530

    Update index.js

--- Node.js/index.js
+++ Node.js/index.js
@@ -3,7 +3,7 @@ const bevatsal1122 = require("./hello.js")
 console.log("\nHello Vatsal\n", bevatsal1122);
 
 
-// To use ED6 Sytanx, Javascript File type should be Module.
+// To use ES6 Syntax, Javascript File Type should be Module
 // It should have extension .mjs
-// You cannot use CommonJS Syntax (require) in .mjs files
-// You cannot use ES^ Syntax (import) in Normal Javascript Files
+// You cannot use CommonJS Syntax (require) in .mjs Files
+// You cannot use ES6 Syntax (import) in Normal Javascript Files
