eefb5572279ee6a8827775eda2454b340e3e5919
Vatsal Sanchala <98870768+bevatsal1122@users.noreply.github.com>
2022-07-20 09:12:06 +0530

    Update About.js

--- capyourtask/src/components/About.js
+++ capyourtask/src/components/About.js
@@ -10,7 +10,7 @@ export const About = () => {
       <h2 className="container"><b>Hello There :) Hope you'all are doing well!!</b></h2>
       <p className="container"><b><big>I'm Vatsal Sanchala, Computer Engg. Student at KJSCE, Mumbai.<br />
       <a href="https://www.github.com/bevatsal1122">GitHub</a><br />
-      <a href="https://www.linkedin.com/in/bevatsal1122/">Linkedin</a>
+      <a href="https://www.linkedin.com/in/bevatsal1122/">LinkedIn</a>
       </big></b></p><br />
       <h4 className="container"><b>Happy Coding 🖤</b></h4>
     </div>
